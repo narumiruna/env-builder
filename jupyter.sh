a88c538c3cb1c8768287b0bfef8bf30030437212
なるみ <weaper@gmail.com>
2018-08-02 11:48:03 +0800

    fix

--- jupyter.sh
+++ jupyter.sh
@@ -3,10 +3,10 @@
 PYTHON=$(which python)
 PYTHON3=$(which python3)
 
-if [ -n $PYTHON3 ]
+if [ -n "$PYTHON3" ]
 then
     $PYTHON3 -m pip install jupyter
-elif [ -n $PYTHON ]
+elif [ -n "$PYTHON" ]
 then
     $PYTHON -m pip install jupyter
 fi
